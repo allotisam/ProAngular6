5f4a0d206486bb53653121b0cff9db343d1c874f
Jeong H Park <hello@jeonghyuk.info>
2019-01-02 10:31:06 -0500

    Adding Angular Features to the Project -Preparing the HTML File -Creating the Data Model -Preparing the Template -Preparing the Component -Putting the Applications Together

---
+++ src/app/model.ts
@@ -0,0 +1,24 @@
+export class Model {
+    user;
+    items;
+
+    constructor() {
+        this.user = 'Jeong';
+        this.items = [
+            new TodoItem('Buy Flowers', false),
+            new TodoItem('Get Shoes', false),
+            new TodoItem('Collect Tickets', true),
+            new TodoItem('Call Joe', false)
+        ];
+    }
+}
+
+export class TodoItem {
+    action: string;
+    done: boolean;
+
+    constructor(action, done) {
+        this.action = action;
+        this.done = done;
+    }
+}

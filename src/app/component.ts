33a7b1a5a6a19cf51f40aa8c581140c6c2894b23
Jeong H Park <hello@jeonghyuk.info>
2019-01-26 10:36:53 -0500

    completed p.274 of 780 Using Data Bindings -Binding Target -Expression -Brackets -Host Element Using the Standard Property and Attribute Bindings -Standard Property Binding -String Interpolation Binding -Attribute Binding Setting Classes and Styles -Class Bindings --Standard Binding --Special Class Binding --ngClass Directive Binding -Style Bindings -Single Style Property --ngStyle Directive Binding

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
 import { Model } from './repository.model';
+import { Product } from './product.model';
 
 @Component({
     selector: 'app',
@@ -7,4 +8,43 @@ import { Model } from './repository.model';
 })
 export class ProductComponent {
     model: Model = new Model();
+
+    fontSizeWithUnits = '30px';
+    fontSizeWithoutUnits = '30';
+
+    constructor(ref: ApplicationRef) {
+        (<any>window).appRef = ref;
+        (<any>window).model = this.model;
+    }
+
+    getClasses(key: number): string {
+        const product = this.model.getProduct(key);
+        return 'p-2 ' + (product.price < 50 ? 'bg-info' : 'bg-warning');
+    }
+
+    getClassMap(key: number): Object {
+        const product = this.model.getProduct(key);
+        return {
+            'text-center bg-danger': product.name === 'Kayak',
+            'bg-info': product.price < 50
+        };
+    }
+
+    getStyles(key: number) {
+        const product = this.model.getProduct(key);
+        return {
+            fontSize: '30px',
+            'margin.px': 100,
+            color: product.price > 50 ? 'red' : 'green'
+        };
+    }
+
+    getProductByPosition(position: number): Product {
+        return this.model.getProducts()[position];
+    }
+
+    getClassesByPosition(position: number): string {
+        const product = this.getProductByPosition(position);
+        return 'p-2 ' + (product.price < 50 ? 'bg-info' : 'bg-warning');
+    }
 }

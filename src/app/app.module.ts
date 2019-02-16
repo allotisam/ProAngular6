4bf7a630e204b966ea9fc18c938c7c5b4a456248
Jeong H Park <hello@jeonghyuk.info>
2019-02-16 18:20:26 -0500

    completed p.376 of 780 Creating a Simple Attribute Directive Accessing Application Data in a Directive Creating Custom Events Creating Host Element Bindings Creating a Two-Way Binding on the Host Element Exporting a Directive for Use in a Template Variable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './component';
+import { PaAttrDirective } from './attr.directive';
+import { PaModel } from './twoway.directive';
 
 @NgModule({
   declarations: [
-    ProductComponent
+    ProductComponent, PaAttrDirective, PaModel
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule

0adda8d7e56ff2ca58b7b9583735608aec824006
Jeong H Park <hello@jeonghyuk.info>
2019-02-19 20:53:40 -0500

    Ch17 - completed p.445 of 780 Querying Template Content

--- src/app/productTable.component.ts
+++ src/app/productTable.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChildren, QueryList } from '@angular/core';
 import { Model } from './repository.model';
 import { Product } from './product.model';
+import { PaCellColor } from './cellColor.directive';
 
 @Component({
     selector: 'paProductTable',
@@ -23,6 +24,27 @@ export class ProductTableComponent {
 
     deleteProduct(key: number) {
         this.dataModel.deleteProduct(key);
+    }
+
+    @ViewChildren(PaCellColor)
+    viewChildren: QueryList<PaCellColor>;
+
+    ngAfterViewInit() {
+        // this allows newly-added views to be applied PaCellColor directive
+        this.viewChildren.changes.subscribe(() => {
+            this.updateViewChildren();
+        });
+
+        // this allows the existing (initially loaded) views to be applied PaCellColor directive
+        this.updateViewChildren();
+    }
+
+    private updateViewChildren() {
+        setTimeout(() => {
+            this.viewChildren.forEach((child, index) => {
+                child.setColor(index % 2 ? true : false);
+            });
+        }, 0);
     }
 
 }

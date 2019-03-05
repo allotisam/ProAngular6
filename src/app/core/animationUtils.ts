0a939498c66f7f99324ff60b4543f7f0a692d863
Jeong H Park <hello@jeonghyuk.info>
2019-03-04 20:16:21 -0500

    Ch28 - completed p748 of 780 Understanding Animation Style Groups -defining common styles in reusable groups -using element transformation -applying CSS framework style

---
+++ src/app/core/animationUtils.ts
@@ -0,0 +1,25 @@
+export function getStylesFromClasses(names: string | string[], elementType: string = 'div'): { [key:string]: string | number } {
+
+    const elem = document.createElement(elementType);
+    (typeof names === 'string' ? [names] : names).forEach(c => elem.classList.add(c));
+
+    const result = {};
+
+    for (let i = 0; i < document.styleSheets.length; i++) {
+        const sheet = document.styleSheets[i] as CSSStyleSheet;
+        const rules = sheet.rules || sheet.cssRules;
+
+        for (let j = 0; j < rules.length; j++) {
+            if (rules[j].type === CSSRule.STYLE_RULE) {
+                const styleRule = rules[j] as CSSStyleRule;
+                if (elem.matches(styleRule.selectorText)) {
+                    for (let k = 0; k < styleRule.style.length; k++) {
+                        result[styleRule.style[k]] = styleRule.style[styleRule.style[k]];
+                    }
+                }
+            }
+        }
+    }
+
+    return result;
+}
--- src/app/core/table.animations.ts
+++ src/app/core/table.animations.ts
@@ -1,33 +1,18 @@
 import { trigger, style, state, transition, animate, group } from '@angular/animations';
+import { getStylesFromClasses } from './animationUtils';
+
+const commonStyles = {
+    border: 'black solid 4px',
+    color: 'white'
+};
 
 export const HighlightTrigger = trigger('rowHighlight', [
-    state('selected', style({
-        backgroundColor: 'lightgreen',
-        fontSize: '20px'
-    })),
-    state('notselected', style({
-        backgroundColor: 'lightsalmon',
-        fontSize: '12px'
-    })),
+    state('selected', style(getStylesFromClasses(['bg-success', 'h2']))),
+    state('notselected', style(getStylesFromClasses('bg-info'))),
     state('void', style({
-        opacity: 0
+        transform: 'translateX(-50%)'
     })),
     transition('* => notselected', animate('200ms')),
-    transition('* => selected',
-        [
-            animate('400ms 200ms ease-in', style({
-                backgroundColor: 'lightblue',
-                fontSize: '25px'
-            })),
-            group([
-                animate('250ms', style({
-                    backgroundColor: 'lightcoral',
-                })),
-                animate('450ms', style({
-                    fontSize: '30px'
-                }))
-            ]),
-            animate('200ms')
-        ]),
+    transition('* => selected', animate('400ms 200ms ease-in')),
     transition('void => *', animate('1000ms'))
 ]);

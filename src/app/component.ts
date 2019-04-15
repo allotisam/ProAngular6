c67c5f845f6e3b85be4da81bc35c9f613952c7dd
Jeong H Park <hello@jeonghyuk.info>
2019-04-15 18:48:36 -0400

    Ch14 - completed p.338 of 780 Working with Forms -adding a form to the example application -adding form data validation ---styling elements using validation classes ---displaying field-level validation messages ---using the component to display validation messages -validating the entire form ---displaying summary validation messages ---disabling the submit button

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,6 +1,9 @@
 import { ApplicationRef, Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { Model } from './repository.model';
 import { Product } from './product.model';
+import { debug } from 'util';
 
 @Component({
     selector: 'app',
@@ -9,6 +12,9 @@ import { Product } from './product.model';
 export class ProductComponent {
     model: Model = new Model();
     selectedProduct: string;
+    newProduct = new Product();
+
+    formSubmitted = false;
 
     getProduct(key: number): Product {
         return this.model.getProduct(key);
@@ -24,5 +30,54 @@ export class ProductComponent {
         }
 
         return false;
+    }
+
+    get jsonProduct() {
+        return JSON.stringify(this.newProduct);
+    }
+
+    addProduct(p: Product) {
+        console.log('New Product: ' + this.jsonProduct);
+    }
+
+    getValidationMessages(state: any, thingName?: string) {
+        debugger;
+        const thing = state.path || thingName;
+        const messages: string[] = [];
+
+        if (state.errors) {
+            for (var error in state.errors) {
+                switch (error) {
+                    case 'required':
+                        messages.push(`You must enter a ${thing}`);
+                        break;
+                    case 'minlength':
+                        messages.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength} characters`);
+                        break;
+                    case 'pattern':
+                        messages.push(`The ${thing} contains illegal characters`);
+                        break;
+                }
+            }
+        }
+        return messages;
+    }
+
+    getFormValidationMessages(form: NgForm): string[] {
+        const messages: string[] = [];
+        Object.keys(form.controls).forEach(k => {
+            this.getValidationMessages(form.controls[k], k).forEach(m => messages.push(m));
+        });
+        return messages;
+    }
+
+    submitForm(form: NgForm) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.reset();
+            this.formSubmitted = false;
+        }
     }
 }

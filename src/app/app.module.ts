5dbdcf8684939296961f374c9a53124dc02e2bf6
Jeong H Park <hello@jeonghyuk.info>
2019-04-15 15:45:28 -0400

    Ch14 - Preparing the Example Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './component';
 
 @NgModule({
+  imports: [
+    BrowserModule, FormsModule
+  ],
   declarations: [
     ProductComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
   ],
-  providers: [],
   bootstrap: [
     ProductComponent
   ]
--- src/app/component.ts
+++ src/app/component.ts
@@ -9,18 +9,6 @@ import { Product } from './product.model';
 export class ProductComponent {
     model: Model = new Model();
 
-    targetName = 'Kayak';
-    counter = 1;
-
-    constructor(ref: ApplicationRef) {
-        (<any>window).appRef = ref;
-        (<any>window).model = this.model;
-    }
-
-    getProductByPosition(position: number): Product {
-        return this.model.getProducts()[position];
-    }
-
     getProduct(key: number): Product {
         return this.model.getProduct(key);
     }
@@ -29,20 +17,4 @@ export class ProductComponent {
         return this.model.getProducts();
     }
 
-    getProductCount(): number {
-        console.log('getProductCount invoked');
-        return this.getProducts().length;
-    }
-
-    getProductPrice(index: number): number {
-        return Math.floor(this.getProduct(index).price);
-    }
-
-    get nextProduct(): Product {
-        return this.model.getProducts().shift();
-    }
-
-    getKey(index: number, product: Product) {
-        return product.id;
-    }
 }
